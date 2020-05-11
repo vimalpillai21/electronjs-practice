261e9bdd0db33723bb2cec4b404166d6b67a0cfd
Vimal Pillai <vimal.pillai@vofoxsolutions.com>
2020-05-11 13:52:04 +0530

    session learned

--- main.js
+++ main.js
@@ -1,65 +1,77 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, session } = require('electron')
 const path = require('path');
 
-// setTimeout(() => {
-//   console.log(app.isReady());
-// },2000);
-
+let mainWindow, secondaryWindow;
 
 function createWindow () {
-  // Create the browser window.
   console.log("Creating window!!!");
-  const mainWindow = new BrowserWindow({
-    width: 1600,
-    height: 900,
+
+  // let customSession = session.fromPartition("part1");
+  // persistent session
+  // let customSession = session.fromPartition("persist:part1");
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    x:100,
+    y:100,
     webPreferences: {
-      // preload: path.join(__dirname, 'preload.js')
       nodeIntegration: true
     }
-  })
+  });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
+  // ------------------------------------
+  secondaryWindow = new BrowserWindow({
+    width: 500,
+    height: 300,
+    x:200,
+    y:200,
+    webPreferences: {
+      nodeIntegration: true,
+      // session: customSession
+      partition: "persist:part1"
+    }
+  });
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  secondaryWindow.loadFile('index.html');
+  // ------------------------------------
+  let ses = mainWindow.webContents.session;
+  // let ses2 = secondaryWindow.webContents.session;
+  ses.clearStorageData();
+  // console.log(Object.is(ses,session.defaultSession));
+  // console.log(ses);
+  // console.log(ses2);
+  // console.log(Object.is(ses,ses2));
+  // console.log(Object.is(ses,customSession));
+
+  mainWindow.on("closed",() => {
+    console.log("main window closed");
+  });
+
+  secondaryWindow.on("closed",() => {
+    console.log("main window closed");
+  });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
 app.on("ready",() =>{
   console.log("App is ready!!!");
 });
 
 app.on("before-quit", (e) => {
   console.log("App is quitting!!!");
-  // e.preventDefault();
 });
 
 app.on("browser-window-focus", () => {
   console.log("app is in focus!!!");
 });
 
-app.on("browser-window-blur", () => {
-  console.log("app is blurred!!!");
-  setTimeout(app.quit,3000);
-});
